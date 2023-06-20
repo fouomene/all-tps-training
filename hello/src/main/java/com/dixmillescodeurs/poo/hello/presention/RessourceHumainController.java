ad56dfd4ff527eb6cd657d181403c0d33526b3ed
fouomene <fouomenedaniel@gmail.com>
2023-06-20 12:08:02 +0200

    Integration Template et Fragmentation avec Thymleaf.

--- hello/src/main/java/com/dixmillescodeurs/poo/hello/presention/RessourceHumainController.java
+++ hello/src/main/java/com/dixmillescodeurs/poo/hello/presention/RessourceHumainController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RessourceHumainController {
@@ -24,6 +23,24 @@ public class RessourceHumainController {
     model.addAttribute("directeurs", directeurs);
 
     return "listedirecteurs";
+  }
+
+
+  @GetMapping("/voyages")
+  public String listevoyages(Model model) {
+
+    var directeurs = ressourceHumaineService.listeDesDirecteurs();
+
+    model.addAttribute("directeurs", directeurs);
+
+
+    return "voyages";
+  }
+
+  @GetMapping("/reservation")
+  public String formulaireReservation(Model model) {
+
+    return "reservation";
   }
 
 }
